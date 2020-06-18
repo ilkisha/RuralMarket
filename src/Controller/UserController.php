320360735461797310c686677c7dd060a527c07f
ilkisha <i.shalvarkov@gmail.com>
2020-06-18 16:23:21 +0300

    Send registration email

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,22 +7,29 @@ use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
     private $passwordEncoder;
+    private $mailer;
 
-    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder,
+                                MailerInterface $mailer)
     {
         $this->passwordEncoder = $passwordEncoder;
+        $this->mailer = $mailer;
     }
 
     /**
      * @Route("/register", name="user_register")
      * @param Request $request
      * @return Response
+     * @throws TransportExceptionInterface
      */
     public function register(Request $request): Response
     {
@@ -31,12 +38,25 @@ class UserController extends AbstractController
         $form->handleRequest($request);
 
         if($form->isSubmitted()) {
-            $passwordHash = $this->passwordEncoder->encodePassword($user, $user->getPassword());
+            $nativePassword = $user->getPassword();
+            $passwordHash = $this->passwordEncoder->encodePassword($user, $nativePassword);
             $user->setPassword($passwordHash);
             $user->setRoles(['ROLE_USER']);
             $manager = $this->getDoctrine()->getManager();
             $manager->persist($user);
             $manager->flush();
+
+            $email = (new Email())
+                ->from('shalvarkohomecamera@gmail.com')
+                ->to($user->getEmail())
+                ->subject('Activate profile')
+                ->text('Your profile is active')
+                ->html('<h1>Hello ' . $user->getFullName() . '</h1></br>'
+                    . 'Username: ' . $user->getEmail() . '</br>'
+                    . 'Password: ' . $nativePassword
+                );
+
+            $this->mailer->send($email);
 
             return $this->redirectToRoute('app_login');
         }
